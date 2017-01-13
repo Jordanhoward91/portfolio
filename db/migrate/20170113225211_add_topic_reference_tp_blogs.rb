34158b49bd5c4f710dcceb2ca2b0730d8d4b2bbc
jordanhoward91 <jordanhoward91>
2017-01-13 15:59:55 -0700

    added migrations

---
+++ db/migrate/20170113225211_add_topic_reference_tp_blogs.rb
@@ -0,0 +1,4 @@
+class AddTopicReferenceTpBlogs < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111203652) do
+ActiveRecord::Schema.define(version: 20170113225211) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -49,6 +49,12 @@ ActiveRecord::Schema.define(version: 20170111203652) do
     t.integer  "percent_utilized"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
