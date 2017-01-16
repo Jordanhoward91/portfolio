1e364b0191ff6fbeeec6c8d6762e40af7bc14f8f
jordanhoward91 <jordanhoward91>
2017-01-16 13:48:52 -0700

    added models, views

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,10 @@
 class Portfolio < ApplicationRecord
+	
+	def self.angular
+		where(subtitle: 'Angular')
+	end
+
+	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on Rails')}
 end
+
+
