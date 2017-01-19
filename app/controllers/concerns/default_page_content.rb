24a1d1ab3e94301b571f349cc0e0345aecda3fe6
jordanhoward91 <jordanhoward91>
2017-01-19 16:47:06 -0700

    Adding custom layouts for blogs and portfolios

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,12 +2,13 @@ module DefaultPageContent
 	extend ActiveSupport::Concern
 
   included do 
-  	before_filter :set_title
+  	before_filter :set_page_defaults
   end
 
 
-	def set_title
+	def set_page_defaults
 		@page_title = "Devcamp Portfolio | My Portfolio Website"
+		@seo_keywords = "Jordan Hudgens portfolio"
 	end  
 end
 
