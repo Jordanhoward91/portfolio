438ddd1ec77fe9795d604d19753faa28217c124f
jordanhoward91 <jordanhoward91>
2017-01-26 15:12:39 -0700

    Updating portfolio to include addr for position

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123220438) do
+ActiveRecord::Schema.define(version: 20170126220629) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170123220438) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "position"
   end
 
   create_table "skills", force: :cascade do |t|
